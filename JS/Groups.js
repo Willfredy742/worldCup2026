0883f9a4174b57d2ed82d5905535caaacdecfa4e
webcartaonline <webcartaonline@gmail.com>
2026-07-14 18:12:09 +0200

    Reconect to repo

---
+++ JS/Groups.js
@@ -0,0 +1,7 @@
+/*
+https://api.football-data.org/v4/competitions/WC/matches?season=2026&stage=GROUP_STAGE
+para el timpo de grupo
+
+https://api.football-data.org/v4/competitions/WC/standings?season=2026
+Para los datos de cada selección
+*/
