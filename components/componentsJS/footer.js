bad2d1ba1b5f29038cf9b93f4f867589a9d16d9b
El-Jesule <gonzalezgomezjesus160619997@gmail.com>
2026-07-20 12:37:53 +0200

    refactor: rename files

---
+++ components/componentsJS/footer.js
@@ -0,0 +1,8 @@
+fetch("components/footer.html")
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById("footer").innerHTML = html;
+  })
+  .catch(error => {
+    console.error("Error al cargar el footer:", error);
+  });
